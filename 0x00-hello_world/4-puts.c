9097f86c987195b4797a051dad8d8f1dccf057b8
Khali3sa <yvonnekhaliesa@gmail.com>
2022-10-15 05:19:57 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
 # !/bin/bash
-gcc -c puts("Programming is like building a multilingual puzzle\n");
+#include <stdio.h>
+/**
+*main - main block
+*
+*Return: 0
+*
+*/
+int main(void)
+{ puts("\"Programming is like building a multilingual puzzle");return (0); }
