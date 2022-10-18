4fff2353a907a148df795ae5c6ac8565219d7a03
Khali3sa <yvonnekhaliesa@gmail.com>
2022-10-17 23:12:33 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *Return: 0 Sucess, non zero fail
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = 1 + 1; j <= 99; j++)
+		{
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+
+			putchar(' ');
+
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
+
+			if (i == 98 && j == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
