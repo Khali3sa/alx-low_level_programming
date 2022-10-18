88995763b5b632b23fc77d50842c6d891dbf6c9e
Khali3sa <yvonnekhaliesa@gmail.com>
2022-10-18 12:38:19 -0700

    My first commmit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet _ prints the alpha
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
