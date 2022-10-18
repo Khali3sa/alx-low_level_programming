e371b3e370aea191fbabc06b856547ca3029a3f2
Khali3sa <yvonnekhaliesa@gmail.com>
2022-10-18 12:41:06 -0700

    My first commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack _bauer(void);
+void jack_bauer(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
