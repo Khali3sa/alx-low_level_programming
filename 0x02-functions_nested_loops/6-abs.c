95e4b9384b6eef8c0b84e357b406c74dda5bd2d9
Khali3sa <yvonnekhaliesa@gmail.com>
2022-10-19 06:32:18 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: The integer to be comuted
+ * Return: The absolute value
+ */
+int _abs(int)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
