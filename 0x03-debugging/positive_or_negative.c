fb635e13bb4f034392c50385eb3e61f1dad2d048
Khali3sa <yvonnekhaliesa@gmail.com>
2022-10-20 12:31:08 -0700

    Debugging commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * @i: argument representing value
+ * positive_or_negative: checks for any positive  of negative number
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+	/* my if statement */
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+}
