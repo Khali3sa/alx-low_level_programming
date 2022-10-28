e8a9c541d50f819cb0a549d02853fcb5df2a8695
Khali3sa <yvonnekhaliesa@gmail.com>
2022-10-28 12:27:30 -0700

    pointers commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,9 +6,5 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *cap_string(char *);
-char *leet(char *a);
-char *string_toupper(char *a);
-int _strcmp(char *s1, char *s2);
+void print_array(int *a, int n);
 int _putchar(char c);
-
-#endif
