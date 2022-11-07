02218ab169890f9754751fcda175dae6d7b9d140
Khali3sa <yvonnekhaliesa@gmail.com>
2022-11-07 06:57:53 -0800

    first commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "main.h" 
+#include "main.h"
 /**
- * main - print the name of the program 
+ * main - print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
  *
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	(void) argv; /*Ignore argv*/ 
+	(void) argv; /*Ignore argv*/
 	printf("%i\n", argc - 1);
 
 	return (0);
