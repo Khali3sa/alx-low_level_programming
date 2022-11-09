fb968fd0ca065be92c49a6d8b9835bae1f956243
Khali3sa <yvonnekhaliesa@gmail.com>
2022-11-09 06:54:12 -0800

    malloc commit

--- 0x0B-malloc_free/_putchar.c
+++ 0x0B-malloc_free/_putchar.c
@@ -1,8 +1,10 @@
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
- * @c: char to be printed
- * Return: 1 on success
+ * @c: The character to print 
+ *
+ * Return:On success 1.
+ * On error, -1 is returned,and errno is set appropriately.
  */
 int _putchar(char c)
 {
