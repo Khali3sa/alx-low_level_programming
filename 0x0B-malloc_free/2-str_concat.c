acbddf0a31c84f22b5067bf3e400b8e10ef2498d
Khali3sa <yvonnekhaliesa@gmail.com>
2022-11-09 10:49:33 -0800

    malloc commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,7 +31,6 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s1[i];
 		i++;
 	}
-	
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,7 +32,6 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	
 	for (x = 0; x < height; x++)
 	{
 		for (y = 0; y < width; y++)
