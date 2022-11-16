20387c53f02200c33ba582124403961f3b24965f
Khali3sa <yvonnekhaliesa@gmail.com>
2022-11-16 01:40:42 -0800

    structures commit

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+struct dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+int _putchar(char c);
+
+#endif
