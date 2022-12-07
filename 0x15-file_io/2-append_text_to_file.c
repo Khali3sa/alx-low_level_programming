c49cea28c1a62bf72a97c9e3f94e78b7b56a2827
Khali3sa <yvonnekhaliesa@gmail.com>
2022-12-07 02:20:28 -0800

    file commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,7 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		rwr = write(fd, text_content, nletters);
 
 		if (rwr == -1)
-			return (-1)
+			return (-1);
 	}
 	close(fd);
 
