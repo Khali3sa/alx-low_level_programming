7ae00010f6d03c781c4c87d60bce9c73ab31e3d5
Khali3sa <yvonnekhaliesa@gmail.com>
2023-01-20 08:09:38 -0800

    dynamic commits

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc *.c -c -fPIC
-gcc *.o -shared -o liball.so
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
